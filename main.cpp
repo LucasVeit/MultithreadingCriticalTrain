a1730cdf7b4ff9065dd2d34289b0b723119c9287
Lucas <lucas-cs1998@hotmail.com>
2022-02-25 09:03:24 -0300

    Finished

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 #include "List.h"
 #include "Item.h"
 //using namespace std;
-#define N_WAGONS 8
-#define MIN_ITEMS 20000000
-#define MAX_ITEMS 30000000
+#define N_WAGONS 2
+#define MIN_ITEMS 10000000
+#define MAX_ITEMS 20000000
 #define SEED 42
 //#define N_THREAD 8
 
@@ -24,14 +24,13 @@ void Populate(List<List<Item>>* train, int n_wagon, int min_items, int max_items
         List<Item> wagon;
         for (int j = 0; j < n_items; j++){
             item.setValue(rand() % 1000);
-            item.setIsLegal(rand() % 2);
+            if(rand() % 2 > 0)
+                item.setIsLegal(1);
+            else
+                item.setIsLegal(0);
             wagon.insert(item);
-            //std::cout << wagon->getData().getFirst()->getData().getValue() << std::endl;
-            //std::cout << node->getData().getValue() << std::endl;
-            //std::cout << wagon->getData().getSize() << std::endl;
         }       
         train->insert(wagon);
-        //std::cout << wagon.getSize() << std::endl;
     }
 }
 
@@ -58,11 +57,10 @@ void PrintIllegal(List<Item>* illegals){
     }
 }
 
-void Serial(List<List<Item>>* train, List<Item>* illegals, int* totalValue){
+void Serial(List<List<Item>>* train, List<Item>* illegals, unsigned long long int* totalValue){
     Node<List<Item>>* wagon = train->getFirst();
     Item item;
     while(wagon != nullptr){
-        //std::cout << "Times " << std::endl;
         Node<Item> *tempNext, *items = wagon->getData().getFirst();
         while(items != nullptr){
             tempNext = items->getNext();
@@ -70,21 +68,21 @@ void Serial(List<List<Item>>* train, List<Item>* illegals, int* totalValue){
                 item = wagon->getData().remove(items);
                 illegals->insert(item);
             }else{
-                totalValue += items->getData().getValue();
+                *totalValue += items->getData().getValue();
             }
             items = tempNext;
         }
         wagon = wagon->getNext();
-        //std::cout << "Wagon" << wagon << std::endl;
     }
 
 }
 
-void IndividualThread(Node<List<Item>>* wagon, List<Item>* illegals, int* totalValue){
+void IndividualThread(Node<List<Item>>* wagon, List<Item>* illegals, unsigned long long int* totalValue){
     unsigned int localValue = 0;
     Item item;
-    Node<Item>* items = wagon->getData().getFirst();
+    Node<Item> *tempNext, *items = wagon->getData().getFirst();
     while(items != nullptr){
+        tempNext = items->getNext();
         if(!items->getData().isLegal()){
             item = wagon->getData().remove(items);
 
@@ -95,15 +93,15 @@ void IndividualThread(Node<List<Item>>* wagon, List<Item>* illegals, int* totalV
             localValue += items->getData().getValue();
         }
 
-        items = items->getNext(); 
+        items = tempNext; 
     }
 
     countingValue.lock();
-    totalValue += localValue;
+    *totalValue += localValue;
     countingValue.unlock();
 }
 
-void Parallel(List<List<Item>>* train, List<Item>* illegals, int* totalValue){
+void Parallel(List<List<Item>>* train, List<Item>* illegals, unsigned long long int* totalValue){
 
     //Controla a criação e   acesso de threads
     Node<List<Item>>* wagon = train->getFirst();
@@ -120,29 +118,27 @@ void Parallel(List<List<Item>>* train, List<Item>* illegals, int* totalValue){
 int main(int argc, char *argv[]){
     List<List<Item>>* train = new List<List<Item>>();
     List<Item>* illegals = new List<Item>();
-    int* totalValue = 0;
+    unsigned long long int totalValue = 0;
 
-    std::cout << "Populating with pseudo-random values..." << std::endl;
+    //std::cout << "Populating with pseudo-random values..." << std::endl;
     Populate(train, N_WAGONS, MIN_ITEMS, MAX_ITEMS);
-    
     if(!strcmp(argv[1], "-Serial")){
-        std::cout << "Executing Serial algorithm..." << std::endl;
+        //std::cout << "Executing Serial algorithm..." << std::endl;
         auto start = std::chrono::high_resolution_clock::now();
-        Serial(train, illegals, totalValue);
+        Serial(train, illegals, &totalValue);
         auto stop = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
         std::cout << "Time taken by Serial function: " << duration.count() << " microseconds" << std::endl;
     }else if(!strcmp(argv[1], "-Parallel")){
-        std::cout << "Executing Parallel algorithm..." << std::endl;
+        //std::cout << "Executing Parallel algorithm..." << std::endl;
         auto start = std::chrono::high_resolution_clock::now();
-        Parallel(train, illegals, totalValue);
+        Parallel(train, illegals, &totalValue);
         auto stop = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
         std::cout << "Time taken by Parallel function: " << duration.count() << " microseconds" << std::endl;
     }else{
         std::cout << "Enter a valid parameter" << std::endl << "-Serial || -Parallel" << std::endl;
     }
-   
     delete train;
     delete illegals;
     
